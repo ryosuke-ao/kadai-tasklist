c5e36ee3649c5261da69203266d937823ec79c95
RT <ace1.rad@gmail.com>
2017-09-16 15:33:41 +0000

    modified twitter clone

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   def index
     @tasks = current_user.tasks.order(created_at: :desc).page(params[:page]).per(10)
   end
